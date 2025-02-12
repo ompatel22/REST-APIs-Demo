73aa81f78057d67c2bd62e70666bb606aab7e1f2
Om Patel <omupatel22@gmail.com>
2025-02-12 17:38:35 +0530

    rest apis demo

---
+++ src/main/java/com/example/restapis/tutorial/RestApIsTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapis.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApIsTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApIsTutorialApplication.class, args);
+	}
+
+}
